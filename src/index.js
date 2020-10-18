68b0fb28a78bc353244c727d65500a33f64e7768
Alexey Golovnev <golovnevalexey@gmail.com>
2020-10-18 20:12:53 +0300

    tiny refactoring

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import {createStore} from "redux";
 import {todoReducer} from "./redux/todoReducer";
 import {composeWithDevTools} from "redux-devtools-extension";
@@ -17,4 +16,3 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('root')
 );
-serviceWorker.unregister();
